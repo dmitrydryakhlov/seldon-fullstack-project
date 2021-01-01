3e1c61298b823c0e3e7bd5b865e15ce43c678724
dmitrydryakhlov <dmitrydryakhlov.mail@gmail.com>
2021-01-01 23:00:54 +0300

    refactoring files structure

--- SeldonTestProject/Backend/dist/utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.random = void 0;
-const random = (min, max) => min + Math.random() * (max - min);
-exports.random = random;
-//# sourceMappingURL=utils.js.map
