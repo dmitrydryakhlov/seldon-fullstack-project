9cf5c1228ed2444f03c16a62993f241d7816ba50
dmitrydryakhlov <dmitrydryakhlov.mail@gmail.com>
2021-01-01 20:25:03 +0300

    docker-compose added

---
+++ SeldonTestProject/Backend/dist/utils.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.random = void 0;
+const random = (min, max) => min + Math.random() * (max - min);
+exports.random = random;
+//# sourceMappingURL=utils.js.map
